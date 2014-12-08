05237fe9fc42f6bf00d351077bf9421bfa9779ad
thiiozz <vermerschth@gmail.com>
2014-12-08 12:08:26 +0100

    Paquet Utils

---
+++ src/main/java/fr/epsi/utils/AbstractLogger.java
@@ -0,0 +1,24 @@
+package fr.epsi.Utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public abstract class AbstractLogger {
+
+    public static void log(String stringToDisplay){
+        System.out.println(logPrefix() + stringToDisplay);
+    }
+
+    private static String logPrefix(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date).toString() + "> ";
+    }
+
+    public static String getConsoleInput(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
---
+++ src/main/java/fr/epsi/utils/XMLParser.java
@@ -0,0 +1,160 @@
+package fr.epsi.Utils;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class XMLParser {
+    private URI usersXMLFile;
+    private URI serverXMLFile;
+
+    private final String nodeUser = "user";
+    private final String attributeUsername = "username";
+    private  final  String attributePassword = "password";
+
+    private final String portNode = "port";
+    private final String timeOutNode = "timeout";
+    private final String baseDirectoryNode = "basedirectory";
+
+    public XMLParser() {
+        this.usersXMLFile = null;
+        this.serverXMLFile = null;
+
+        setServerXMLFile();
+        setUsersXMLFile();
+    }
+
+    private void setServerXMLFile() {
+        try {
+            this.serverXMLFile = getClass().getClassLoader().getResource("server-configuration.xml").toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUsersXMLFile(){
+        try {
+            this.usersXMLFile = getClass().getClassLoader().getResource("users.xml").toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean userXMLFileIsValid(){
+        return this.usersXMLFile != null;
+    }
+
+    private boolean serverXMLFileIsValid(){
+        return this.serverXMLFile != null;
+    }
+
+    public synchronized String serverPort(){
+        String serverPort = "";
+
+        if(serverXMLFileIsValid()){
+            try {
+                File fXmlFile = new File(serverXMLFile);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(fXmlFile);
+
+                doc.getDocumentElement().normalize();
+                NodeList portNodeList = doc.getElementsByTagName(portNode);
+
+                serverPort = portNodeList.item(0).getTextContent();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return serverPort;
+    }
+
+    public synchronized String serverTimeOut(){
+        String timeOut = "";
+
+        if(serverXMLFileIsValid()){
+            try {
+                File fXmlFile = new File(serverXMLFile);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(fXmlFile);
+
+                doc.getDocumentElement().normalize();
+                NodeList timeOutNodeList = doc.getElementsByTagName(timeOutNode);
+
+                timeOut = timeOutNodeList.item(0).getTextContent();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return timeOut;
+    }
+
+    public synchronized String serverBaseDirectory(){
+        String serverBaseDirectory = "";
+
+        if(serverXMLFileIsValid()){
+            try {
+                File fXmlFile = new File(serverXMLFile);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(fXmlFile);
+
+                doc.getDocumentElement().normalize();
+                NodeList timeOutNodeList = doc.getElementsByTagName(baseDirectoryNode);
+
+                serverBaseDirectory = timeOutNodeList.item(0).getTextContent();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return serverBaseDirectory;
+    }
+
+    public synchronized Map<String, String> parseAndGetUsersXMLFile(){
+        Map<String, String> credentials = new HashMap<String, String>();
+
+        if(userXMLFileIsValid()){
+            try {
+                File fXmlFile = new File(usersXMLFile);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(fXmlFile);
+
+                doc.getDocumentElement().normalize();
+                NodeList usersNodeList = doc.getElementsByTagName(nodeUser);
+
+                for (int i = 0; i < usersNodeList.getLength(); i++){
+                    Node userNode = usersNodeList.item(i);
+
+                    if (userNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element userElement = (Element) userNode;
+                        credentials.put(userElement.getAttribute(attributeUsername), userElement.getAttribute(attributePassword));
+                    }
+
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(credentials == null || credentials.isEmpty()){
+            credentials = Collections.emptyMap();
+        }
+
+        return credentials;
+    }
+
+}
