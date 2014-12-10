4506f26957af28529199cbdcab6829b2bb52f3cc
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2014-12-10 12:25:40 +0100

    Merging

--- src/test/java/fr/epsi/server/core/ServerTest.java
+++ src/test/java/fr/epsi/server/core/ServerTest.java
@@ -1,23 +1,34 @@
 package fr.epsi.server.core;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 
 public class ServerTest extends TestCase {
 
-    public void setUp() throws Exception {
-        super.setUp();
+    private Server server;
 
+    @Before
+    public void setUp() throws Exception {
+        server = new Server();
     }
 
+    @After
     public void tearDown() throws Exception {
+        server.stopServer();
 
+        assertTrue(server.getServerSocket().isClosed());
+        assertFalse(server.getListeningThread().isAlive());
+    }
+
+    public void testInitialization() throws Exception {
+        assertTrue(server.getServerSocket().isBound());
+        assertFalse(server.getListeningThread().isAlive());
     }
 
     public void testStartServer() throws Exception {
+        server.startServer();
 
-    }
-
-    public void testStopServer() throws Exception {
-
+        assertTrue(server.getListeningThread().isAlive());
     }
 }
