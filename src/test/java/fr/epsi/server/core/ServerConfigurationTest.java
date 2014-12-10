3b93ed6c539b56bbedb4f3198ca4f2029bb22105
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2014-12-10 12:04:43 +0100

    ServerConfiguration tested

---
+++ src/test/java/fr/epsi/server/core/ServerConfigurationTest.java
@@ -0,0 +1,19 @@
+package fr.epsi.server.core;
+
+import junit.framework.TestCase;
+
+public class ServerConfigurationTest extends TestCase {
+
+    private ServerConfiguration serverConfiguration;
+
+    @Override
+    public void setUp() throws Exception {
+        this.serverConfiguration = new ServerConfiguration();
+    }
+
+    public void testConfigurationLoading() throws Exception {
+        assertEquals(this.serverConfiguration.serverPort(), 4002);
+        assertEquals(this.serverConfiguration.serverTimeOut(), 3500);
+        assertEquals(this.serverConfiguration.serverBaseDirectory(), "D:/FTPFolder");
+    }
+}
---
+++ src/test/java/fr/epsi/server/core/ServerTest.java
@@ -0,0 +1,23 @@
+package fr.epsi.server.core;
+
+import junit.framework.TestCase;
+
+public class ServerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testStartServer() throws Exception {
+
+    }
+
+    public void testStopServer() throws Exception {
+
+    }
+}
